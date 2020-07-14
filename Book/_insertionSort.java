d33d500cc11c8886d7ca78ba4d5f8bf8be39f27a
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-15 04:49:52 +0530

    Strassen's Algorithm added

--- Book/_insertionSort.java
+++ Book/_insertionSort.java
@@ -1,36 +1,81 @@
-package Book;
-
 import java.util.Scanner;
+
 class _insertionSort {
+
     public static void main(String[]args)throws java.lang.Exception {
+
         Scanner sc = new Scanner(System. in);
+
         int size = sc.nextInt();
+
         int arr[] = new int[size];
 
-        arr[0] = sc.nextInt();
-        for (int i = 1; i < size; i++) {
-            int element = sc.nextInt();
-            int j = i - 1;
-            while (j >= 0 && arr[j] > element) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = element;
+        for (int i = 0; i < size; i++) {
+
+            arr[i] = sc.nextInt();
+
         }
+
+        int result[] = insertionSort(arr);
+
+        System
+            .out
+            .println("Input Array:");
 
         display(arr);
 
+        System
+            .out
+            .println("Output Array:");
+
+        display(result);
+
         sc.close();
+
+    }
+
+    private static int[]insertionSort(int A[]) {
+
+        int arr[] = new int[A.length];
+
+        arr[0] = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+
+            int element = A[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > element) {
+
+                arr[j + 1] = arr[j];
+
+                j--;
+
+            }
+
+            arr[j + 1] = element;
+
+        }
+
+        return arr;
+
     }
 
     private static void display(int[]arr) {
+
         for (int i = 0; i < arr.length; i++) {
+
             System
                 .out
                 .print(arr[i] + " ");
+
         }
+
         System
             .out
             .println();
+
     }
+
 }
--- Book/_mergeSort.java
+++ Book/_mergeSort.java
@@ -1,55 +1,92 @@
-package Book;
-
 import java.util.Scanner;
 
 class _mergeSort {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int arr[] = new int[size];
-        for (int i = 0; i < size; i++)
-            arr[i] = sc.nextInt();
-        divide(arr, 0, size - 1);
-        for (int i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-        sc.close();
+
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int size = sc.nextInt();
+
+    int arr[] = new int[size];
+
+    for (int i = 0; i < size; i++) arr[i] = sc.nextInt();
+
+    divide(arr, 0, size - 1);
+
+    display(arr);
+
+    sc.close();
+  }
+
+  public static void divide(int arr[], int start, int end) {
+
+    if (start < end) {
+
+      int mid = (int) Math.floor((end + start) / 2);
+
+      divide(arr, start, mid);
+
+      divide(arr, mid + 1, end);
+
+      merge(arr, start, mid, end);
+
     }
 
-    public static void divide(int arr[], int start, int end) {
-        if (start < end) {
-            int mid = (int) Math.floor((end + start) / 2);
-            divide(arr, start, mid);
-            divide(arr, mid + 1, end);
-            merge(arr, start, mid, end);
-        }
+  }
+
+  public static void merge(int arr[], int start, int mid, int end) {
+
+    int len1 = mid - start + 1;
+
+    int len2 = end - mid;
+
+    int L[] = new int[len1];
+
+    int R[] = new int[len2];
+
+    for (int i = 0; i < len1; i++) L[i] = arr[i + start];
+
+    for (int j = 0; j < len2; j++) R[j] = arr[j + mid + 1];
+
+    int i = 0, j = 0, k = start;
+
+    while (i < len1 && j < len2) {
+
+      if (L[i] < R[j]) {
+
+        arr[k] = L[i];
+
+        i++;
+
+      } else {
+
+        arr[k] = R[j];
+
+        j++;
+
+      }
+
+      k++;
+
     }
 
-    public static void merge(int arr[], int start, int mid, int end) {
-        int len1 = mid - start + 1;
-        int len2 = end - mid;
-        int L[] = new int[len1];
-        int R[] = new int[len2];
-        for (int i = 0; i < len1; i++)
-            L[i] = arr[i + start];
-        for (int j = 0; j < len2; j++)
-            R[j] = arr[j + mid + 1];
-        int i = 0, j = 0, k = start;
-        while (i < len1 && j < len2) {
-            if (L[i] < R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
+    while (i < len1) arr[k++] = L[i++];
 
-        while (i < len1)
-            arr[k++] = L[i++];
+    while (j < len2) arr[k++] = R[j++];
 
-        while (j < len2)
-            arr[k++] = R[j++];
+  }
+
+  private static void display(int[] arr) {
+
+    for (int i = 0; i < arr.length; i++) {
+
+      System.out.print(arr[i] + " ");
+
     }
+
+    System.out.println();
+
+  }
+  
 }
