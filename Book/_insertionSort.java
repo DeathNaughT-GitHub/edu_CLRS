76aa86d9e9d85843c4fe09388644d7b913a29315
DeathNaught-GitHub <coding.ayush@gmail.com>
2020-07-15 19:42:16 +0530

    Code Review

--- Book/_insertionSort.java
+++ Book/_insertionSort.java
@@ -2,56 +2,65 @@ import java.util.Scanner;
 
 class _insertionSort {
 
-  static Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System. in);
 
-  public static void main(String[] args) throws java.lang.Exception {
+    public static void main(String[]args)throws java.lang.Exception {
 
-    int size = sc.nextInt();
-    int arr[] = readArray(size);
+        int size = sc.nextInt();
+        int arr[] = readArray(size);
 
-    int result[] = insertionSort(arr);
+        int result[] = insertionSort(arr);
 
-    System.out.println("Input Array:");
-    display(arr);
+        System
+            .out
+            .println("Input Array:");
+        display(arr);
 
-    System.out.println("Output Array:");
-    display(result);
+        System
+            .out
+            .println("Output Array:");
+        display(result);
 
-    sc.close();
-  }
-
-  private static int[] insertionSort(int A[]) {
-    int arr[] = new int[A.length];
-
-    arr[0] = A[0];
-    for (int i = 1; i < A.length; i++) {
-      int element = A[i];
-      int j = i - 1;
-
-      while (j >= 0 && arr[j] > element) {
-        arr[j + 1] = arr[j];
-        j--;
-      }
-
-      arr[j + 1] = element;
+        sc.close();
     }
 
-    return arr;
-  }
+    private static int[]insertionSort(int A[]) {
+        int arr[] = new int[A.length];
 
-  private static void display(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + " ");
+        arr[0] = A[0];
+        for (int i = 1; i < A.length; i++) {
+            int element = A[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > element) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = element;
+        }
+
+        return arr;
     }
 
-    System.out.println();
-  }
+    private static void display(int[]arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System
+                .out
+                .print(arr[i] + " ");
+        }
 
-  private static int[] readArray(int size) {
-    int arr[] = new int[size];
-
-    for (int i = 0; i < size; i++) {
-      arr[i] = sc.nextInt();
+        System
+            .out
+            .println();
     }
-  }
+
+    private static int[]readArray(int size) {
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
 }
